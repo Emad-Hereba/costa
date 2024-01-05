0e258122896ef580ce09a4af43c3271122a96324
Emad Hereba <tuguiaegipcio@gmail.com>
2024-01-05 22:15:41 +0200

    lastone

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { createRoot } from 'react-dom/client';
+import { Form, Field, FormElement } from '@progress/kendo-react-form';
 
-function App() {
-  const [count, setCount] = useState(0)
+import {
+      FormInput,
+      
+} from './form-components';
 
-  return (
-    <>
-      <div>
-       <h1>Vite + React+ Type Script (starting 4 Jan 2024)</h1>
-      </div>
-    </>
-  )
-}
+import {
+    emailValidator, nameValidator,
+     
+    
+} from './validators'
 
-export default App
+const App = () => {
+    const handleSubmit = (dataItem: {[name: string]: any}) => alert(JSON.stringify(dataItem, null, 2));
+    return (
+      <Form
+        onSubmit={handleSubmit}
+        render={() => (
+          <FormElement style={{width: 400}}>
+            <fieldset className={'k-form-fieldset'}>
+              <legend className={'k-form-legend'}>BOOK YOUR DREAM VACATION TODAY</legend>
+              <Field
+                id={'fullName'}
+                name={'fullName'}
+                label={'Full Name'}
+                component={FormInput}
+                validator={nameValidator}
+                        />
+              <Field
+                id={'email'}
+                name={'email'}
+                label={'Email'}
+                hint={'Hint: Enter your personal email address.'}
+                type={'email'}
+                component={FormInput}
+                validator={emailValidator}
+                        />
+              <div style={{display: 'flex', justifyContent: 'space-between'}}>
+              </div>
+            </fieldset>
+          </FormElement>
+            )}
+        />
+    );
+    
+
+  };
+  const domNode=document.getElementById('root');
+  const root=createRoot(domNode);
+  root.render(<App />);
+
+export default App;
---
+++ src/form-components.tsx
@@ -0,0 +1,161 @@
+import * as React from "react";
+
+import { FieldRenderProps, FieldWrapper } from "@progress/kendo-react-form";
+import {
+  Input,
+  
+  
+  Checkbox,
+  RadioGroup,
+} from "@progress/kendo-react-inputs";
+import {
+  Label,
+  Error,
+  Hint,
+  
+} from "@progress/kendo-react-labels";
+
+export const FormInput = (fieldRenderProps: FieldRenderProps) => {
+  const {
+    validationMessage,
+    touched,
+    label,
+    id,
+    valid,
+    disabled,
+    hint,
+    type,
+    optional,
+    ...others
+  } = fieldRenderProps;
+
+  const showValidationMessage: string | false | null =
+    touched && validationMessage;
+  const showHint: boolean = !showValidationMessage && hint;
+  const hintId: string = showHint ? `${id}_hint` : "";
+  const errorId: string = showValidationMessage ? `${id}_error` : "";
+
+  return (
+    <FieldWrapper>
+      <Label
+        editorId={id}
+        editorValid={valid}
+        editorDisabled={disabled}
+        optional={optional}
+        className="k-form-label"
+      >
+        {label}
+      </Label>
+      <div className={"k-form-field-wrap"}>
+        <Input
+          valid={valid}
+          type={type}
+          id={id}
+          disabled={disabled}
+          ariaDescribedBy={`${hintId} ${errorId}`}
+          {...others}
+        />
+        {showHint && <Hint id={hintId}>{hint}</Hint>}
+        {showValidationMessage && (
+          <Error id={errorId}>{validationMessage}</Error>
+        )}
+      </div>
+    </FieldWrapper>
+  );
+};
+
+export const FormCheckbox = (fieldRenderProps: FieldRenderProps) => {
+  const {
+    validationMessage,
+    touched,
+    id,
+    valid,
+    disabled,
+    hint,
+    optional,
+    label,
+    visited,
+    modified,
+    ...others
+  } = fieldRenderProps;
+
+  const showValidationMessage: string | false | null =
+    touched && validationMessage;
+  const showHint: boolean = !showValidationMessage && hint;
+  const hintId: string = showHint ? `${id}_hint` : "";
+  const errorId: string = showValidationMessage ? `${id}_error` : "";
+
+  return (
+    <FieldWrapper>
+      <div className={"k-form-field-wrap"}>
+        <Checkbox
+          ariaDescribedBy={`${hintId} ${errorId}`}
+          label={label}
+          labelOptional={optional}
+          valid={valid}
+          id={id}
+          disabled={disabled}
+          labelClassName="k-form-label"
+          {...others}
+        />
+        {showHint && <Hint id={hintId}>{hint}</Hint>}
+        {showValidationMessage && (
+          <Error id={errorId}>{validationMessage}</Error>
+        )}
+      </div>
+    </FieldWrapper>
+  );
+};
+
+
+export const FormRadioGroup = (fieldRenderProps: FieldRenderProps) => {
+  const {
+    validationMessage,
+    touched,
+    id,
+    label,
+    valid,
+    disabled,
+    hint,
+    visited,
+    modified,
+    ...others
+  } = fieldRenderProps;
+  const editorRef = React.useRef<any>(null);
+
+  const showValidationMessage: string | false | null =
+    touched && validationMessage;
+  const showHint: boolean = !showValidationMessage && hint;
+  const hintId: string = showHint ? `${id}_hint` : "";
+  const errorId: string = showValidationMessage ? `${id}_error` : "";
+  const labelId: string = label ? `${id}_label` : "";
+
+  return (
+    <FieldWrapper>
+      <Label
+        id={labelId}
+        editorRef={editorRef}
+        editorId={id}
+        editorValid={valid}
+        editorDisabled={disabled}
+        className="k-form-label"
+      >
+        {label}
+      </Label>
+      <div className={"k-form-field-wrap"}>
+        <RadioGroup
+          ariaDescribedBy={`${hintId} ${errorId}`}
+          ariaLabelledBy={labelId}
+          valid={valid}
+          disabled={disabled}
+          ref={editorRef}
+          {...others}
+        />
+        {showHint && <Hint id={hintId}>{hint}</Hint>}
+        {showValidationMessage && (
+          <Error id={errorId}>{validationMessage}</Error>
+        )}
+      </div>
+    </FieldWrapper>
+  );
+};

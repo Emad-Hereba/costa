5832b388edf98158fab1456a05571ef9ae8f7f5c
Emad Hereba <tuguiaegipcio@gmail.com>
2024-01-05 16:55:32 +0200

    error creat root

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+       <h1>Vite + React+ Type Script (starting 4 Jan 2024)</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
